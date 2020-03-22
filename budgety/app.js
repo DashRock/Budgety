30aecbdd600cb23e99b2ba3711f079d7df2b6b39
DashRock <30941886+DashRock@users.noreply.github.com>
2020-03-21 20:34:52 -0700

    Add files via upload

--- budgety/app.js
+++ budgety/app.js
@@ -57,10 +57,6 @@ var budgetController = (function() {
         addItem: function(type, des, val) {
             var newItem, ID;
 
-            //[1 2 3 4 5], next ID = 6
-            //[1 2 4 6 8], next ID = 9
-            // ID = last ID + 1
-
             // Create new ID
             if (data.allItems[type].length > 0) {
                 ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
@@ -86,12 +82,7 @@ var budgetController = (function() {
         deleteItem: function(type, id) {
             var ids, index;
 
-            // id = 6
-            //data.allItems[type][id];
-            // ids = [1 2 4  8]
-            //index = 3
-
-            ids = data.allItems[type].map(function(current) {
+           ids = data.allItems[type].map(function(current) {
                 return current.id;
             });
 
@@ -125,17 +116,7 @@ var budgetController = (function() {
 
         calculatePercentages: function() {
 
-            /*
-            a=20
-            b=10
-            c=40
-            income = 100
-            a=20/100=20%
-            b=10/100=10%
-            c=40/100=40%
-            */
-
-            data.allItems.exp.forEach(function(cur) {
+          data.allItems.exp.forEach(function(cur) {
                 cur.calcPercentage(data.totals.inc);
             });
         },
@@ -190,15 +171,7 @@ var UIController = (function() {
 
             var formatNumber = function(num, type) {
                 var numSplit, int, dec, type;
-                /*
-                    + or - before number
-                    exactly 2 decimal points
-                    comma separating the thousands
-
-                    2310.4567 -> + 2,310.46
-                    2000 -> + 2,000.00
-                    */
-
+                
                 num = Math.abs(num);
                 num = num.toFixed(2);
 
